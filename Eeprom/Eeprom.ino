e555566dfbea7eb88d147ce180594c006425a0b6
Zero <dinismatej1@gmail.com>
2021-10-23 16:02:47 +0200

    .

--- Eeprom/Eeprom.ino
+++ Eeprom/Eeprom.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
 
+unsigned char input_data[] = {
+    
+};
+
+int address_pins[13];
+int IO_pins[8];
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(13, LOW);
 
+  digitalWrite(13, HIGH);
+  delay(100000000);
 }
