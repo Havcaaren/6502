47d3d807a67a02d7aa72dd3116e3e297221e0a70
Zero <dinismatej1@gmail.com>
2021-12-21 22:48:10 +0100

    Started to completely reworking RS.

--- Rosetta Stone/main.cpp
+++ Rosetta Stone/main.cpp
@@ -1,36 +1,112 @@
 #include <iostream>
 
-#include "Rosetta_Stone.h"
+#include <list>
+#include <fstream>
+#include <algorithm>
+#include <map>
 
-int main(int argc, char** argv) {
+struct section {
+    std::list<std::string> *data;
+    std::list<std::string> *program;
+};
+
+std::list<std::string> *normalized_and_split(std::ifstream *);
+
+section split(std::list<std::string> *);
+
+std::list<std::string>* give_data_pos(std::list<std::string>*);
+
+std::list<std::string>* give_label_pos(std::list<std::string>*);
+
+int main(int argc, char **argv) {
     if (argc == 1) {
-        std::cout<<"No input file."<<std::endl;
+        std::cout << "No input file." << std::endl;
         return -1;
     }
 
     bool opt = false;
-    if (argc == 2) {
-        opt = true;
-    }
+//    if (argc == 2) {
+//        opt = true;
+//    }
+
+    auto input_file = new std::ifstream;
+    std::list<std::string> *nz_file = nullptr;
 
     // "..\\test.asm" argv[1]
-    Lexer lx("..\\test.asm");
-    lx.tokenized();
-    Rosetta_Stone RS(opt);
-    RS.setInstructions(lx.getInstructions());
-    RS.optimized();
-    RS.compile();
-    std::cout<<"A";
-//    ps.findOrg();
-//    ps.findLabels();
+    input_file->open("..\\test.asm");
 
-//    try {
-//        Parser ps(argv[1]);
-//        //ps.parse();
-//        //ps.print();
-//    } catch (std::invalid_argument& e) {
-//        std::cerr<<e.what()<<std::endl;
-//        return -3;
+    auto [data, program] = split(normalized_and_split(input_file));
+
+//    nz_file = normalized_and_split(input_file);
+//    auto [data, program] = split(nz_file);
+
+//    for (const auto &i: *nz_file) {
+//        std::cout << i << "\n";
 //    }
+    std::cout << "---------\n";
+    for (const auto &i: *data) {
+        std::cout << i << "\n";
+    }
+    std::cout << "---------\n";
+    for (const auto &i: *program) {
+        std::cout << i << "\n";
+    }
+    std::cout << "---------\n";
+
+
+
     return 0;
 }
+
+std::list<std::string> *normalized_and_split(std::ifstream *in) {
+    auto ld = new std::list<std::string>;
+    std::string buffer((std::istreambuf_iterator<char>(*in)),
+                       (std::istreambuf_iterator<char>()));
+
+    for (int i = 0; i < buffer.length(); ++i) {
+        if (buffer[i] == '\n') {
+            buffer[i] = ' ';
+            i--;
+        }
+        if (buffer[i] == ' ' && buffer[i + 1] == ' ') {
+            buffer.erase(i, 1);
+            i--;
+        }
+    }
+    if (buffer[0] == ' ') {
+        buffer.erase(0, 1);
+    }
+    for (int i = 0; i < buffer.length(); ++i) {
+        if (i == buffer.length() - 1) {
+            ld->push_back(buffer.substr(0, buffer.length()));
+        } else if (buffer[i] == ' ') {
+            ld->push_back(buffer.substr(0, i));
+            buffer.erase(0, i + 1);
+            i = 0;
+        }
+    }
+    return ld;
+}
+
+section split(std::list<std::string> *ld) {
+    section st{
+            .data = new std::list<std::string>,
+            .program = new std::list<std::string>
+    };
+    st.data->splice(st.data->begin(),
+                    *ld,
+                    ++ld->begin(),
+                    (std::find(ld->begin(), ld->end(), ".program:")));
+    st.program->splice(st.program->begin(),
+                       *ld,
+                       ++(std::find(ld->begin(), ld->end(), ".program:")),
+                       ld->end());
+    delete ld;
+    return st;
+}
+
+std::list<std::string>* give_data_pos(std::list<std::string>* ld) {
+
+}
+
+std::list<std::string>* give_label_pos(std::list<std::string>* ld) {}
