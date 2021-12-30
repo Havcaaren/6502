29552b3f6c6630cb4b04a4ebec3ac912903cb8cf
Zero <dinismatej1@gmail.com>
2021-12-30 16:49:11 +0100

    TODO finish insert and writing to out.bin.

--- Rosetta Stone/main.cpp
+++ Rosetta Stone/main.cpp
@@ -25,16 +25,29 @@ struct op_code {
     int size;
 };
 
-std::list<std::string> *normalized_and_split(std::ifstream *);
+std::map<std::string, op_code> *create_OP_map();
 
-section split(std::list<std::string> *);
+std::list<std::string> *normalized_and_split(std::ifstream *file);
 
-std::list<data*> *give_data_pos(std::list<std::string> *);
+section split(std::list<std::string> *file);
 
-std::list<label*> *give_label_pos(std::list<std::string> *,
-                                  std::map<std::string, int> *);
+std::list<std::string> *find_labels(std::list<std::string> *program);
 
-std::map<std::string, op_code>* create_OP_map();
+std::list<data *> *give_data_pos(std::list<std::string> *dataL);
+
+std::list<label *> *give_label_pos(std::list<std::string> *program,
+                                   std::map<std::string, op_code> *OPs);
+
+std::list<std::string> *remove_labels(std::list<std::string> *program,
+                                      std::list<label *> *label_list);
+
+std::list<label *> *normalized_labels(std::list<label *> *label_list);
+
+std::string add_padding(std::string s);
+
+std::list<std::string> *insert_addresses(std::list<std::string> *program,
+                                         std::list<data *> *data_list,
+                                         std::list<label *> *labels);
 
 int main(int argc, char **argv) {
     if (argc == 1) {
@@ -55,6 +68,8 @@ int main(int argc, char **argv) {
 
     auto[data, program] = split(normalized_and_split(input_file));
 
+
+
 //    nz_file = normalized_and_split(input_file);
 //    auto [data, program] = split(nz_file);
 
@@ -70,76 +85,27 @@ int main(int argc, char **argv) {
         std::cout << i << "\n";
     }
     std::cout << "---------\n";
-    auto m = create_OP_map();
 
+    auto A = give_data_pos(data);
+    auto B = give_label_pos(program, create_OP_map());
+    std::cout << "---------\n";
+    for (auto i: *A) {
+        std::cout << i->name << " " << i->pos << "\n";
+    }
+    std::cout << "---------\n";
+    for (auto i: *B) {
+        std::cout << i->name << " " << i->pos << "\n";
+    }
+    std::cout << "---------\n";
+    auto C = remove_labels(program, B);
+    for (const auto &i: *C) {
+        std::cout << i << "\n";
+    }
     return 0;
 }
 
-std::list<std::string> *normalized_and_split(std::ifstream *in) {
-    auto ld = new std::list<std::string>;
-    std::string buffer((std::istreambuf_iterator<char>(*in)),
-                       (std::istreambuf_iterator<char>()));
-
-    for (int i = 0; i < buffer.length(); ++i) {
-        if (buffer[i] == '\n') {
-            buffer[i] = ' ';
-            i--;
-        }
-        if (buffer[i] == ' ' && buffer[i + 1] == ' ') {
-            buffer.erase(i, 1);
-            i--;
-        }
-    }
-    if (buffer[0] == ' ') {
-        buffer.erase(0, 1);
-    }
-    for (int i = 0; i < buffer.length(); ++i) {
-        if (i == buffer.length() - 1) {
-            ld->push_back(buffer.substr(0, buffer.length()));
-        } else if (buffer[i] == ' ') {
-            ld->push_back(buffer.substr(0, i));
-            buffer.erase(0, i + 1);
-            i = 0;
-        }
-    }
-    return ld;
-}
-
-section split(std::list<std::string> *ld) {
-    section st{
-            .data = new std::list<std::string>,
-            .program = new std::list<std::string>
-    };
-    st.data->splice(st.data->begin(),
-                    *ld,
-                    ++ld->begin(),
-                    (std::find(ld->begin(), ld->end(), ".program:")));
-    st.program->splice(st.program->begin(),
-                       *ld,
-                       ++(std::find(ld->begin(), ld->end(), ".program:")),
-                       ld->end());
-    delete ld;
-    return st;
-}
-
-std::list<data*> *give_data_pos(std::list<std::string> *ld) {
-    auto* data_list = new std::list<data*>;
-    int p = 0;
-    for (const auto& i: *ld) {
-        data_list->push_back(new data {
-            .name = i,
-            .pos = p
-        });
-        p++;
-    }
-    return data_list;
-}
-
-std::list<label*> *give_label_pos(std::list<std::string> *ld,
-                                  std::map<std::string, int>* OP) {}
-
-std::map<std::string, op_code>* create_OP_map() {
-    auto* map = new std::map<std::string, op_code>;
+std::map<std::string, op_code> *create_OP_map() {
+    auto *map = new std::map<std::string, op_code>;
     map->insert({"NOP", {0x80, 1}});
     map->insert({"CLX", {0x81, 1}});
     map->insert({"CLY", {0x82, 1}});
@@ -202,4 +168,158 @@ std::map<std::string, op_code>* create_OP_map() {
     map->insert({"NOA", {0x46, 1}});
     map->insert({"NOV", {0x47, 2}}); // why????
     return map;
+}
+
+std::list<std::string> *normalized_and_split(std::ifstream *file) {
+    auto ld = new std::list<std::string>;
+    std::string buffer((std::istreambuf_iterator<char>(*file)),
+                       (std::istreambuf_iterator<char>()));
+
+    for (int i = 0; i < buffer.length(); ++i) {
+        if (buffer[i] == '\n') {
+            buffer[i] = ' ';
+            i--;
+        }
+        if (buffer[i] == ' ' && buffer[i + 1] == ' ') {
+            buffer.erase(i, 1);
+            i--;
+        }
+    }
+    if (buffer[0] == ' ') {
+        buffer.erase(0, 1);
+    }
+    for (int i = 0; i < buffer.length(); ++i) {
+        if (i == buffer.length() - 1) {
+            ld->push_back(buffer.substr(0, buffer.length()));
+        } else if (buffer[i] == ' ') {
+            ld->push_back(buffer.substr(0, i));
+            buffer.erase(0, i + 1);
+            i = 0;
+        }
+    }
+    return ld;
+}
+
+section split(std::list<std::string> *file) {
+    section st{
+            .data = new std::list<std::string>,
+            .program = new std::list<std::string>
+    };
+    st.data->splice(st.data->begin(),
+                    *file,
+                    ++file->begin(),
+                    (std::find(file->begin(), file->end(), ".program:")));
+    st.program->splice(st.program->begin(),
+                       *file,
+                       ++(std::find(file->begin(), file->end(), ".program:")),
+                       file->end());
+    delete file;
+    return st;
+}
+
+std::list<std::string> *find_labels(std::list<std::string> *program) {
+    auto *list = new std::list<std::string>;
+    for (const auto &i: *program) {
+        if (i.find(':') != std::string::npos) {
+            list->push_back(i);
+        }
+    }
+    return list;
+}
+
+std::list<data *> *give_data_pos(std::list<std::string> *dataL) {
+    auto *data_list = new std::list<data *>;
+    int p = 0;
+    for (const auto &i: *dataL) {
+        data_list->push_back(new data{
+                .name = i,
+                .pos = p
+        });
+        p++;
+    }
+    return data_list;
+}
+
+std::list<label *> *give_label_pos(std::list<std::string> *program,
+                                   std::map<std::string, op_code> *OPs) {
+    auto *list = new std::list<label *>;
+    auto *labels = find_labels(program);
+    int index = 0;
+    for (const auto &i: *program) {
+        auto it_label = std::find_if(labels->begin(),
+                                     labels->end(),
+                                     [&](const std::string &A) { return i == A; });
+        auto it_op = OPs->find(i);
+        if (it_label != labels->end()) {
+            list->push_back(new label{
+                    .name = (*it_label),
+                    .pos = index
+            });
+        } else if (it_op != OPs->end()) {
+            index += it_op->second.size;
+        }
+    }
+    delete labels, OPs;
+    return list;
+}
+
+std::list<std::string> *remove_labels(std::list<std::string> *program,
+                                      std::list<label *> *label_list) {
+    auto *new_ld = new std::list<std::string>;
+    for (auto i: *program) {
+        auto it_label = std::find_if(label_list->begin(),
+                                     label_list->end(),
+                                     [&](label *A) { return i == A->name; });
+        if (it_label == label_list->end()) {
+            new_ld->push_back(i);
+        }
+    }
+    delete program;
+    return new_ld;
+}
+
+std::list<label *> *normalized_labels(std::list<label *> *label_list) {
+    for (auto i: *label_list) {
+        i->name = i->name.substr(0, i->name.length() - 1);
+    }
+    return label_list;
+}
+
+std::string add_padding(std::string s) {
+    std::reverse(s.begin(), s.end());
+    while (s.length() != 4) {
+        s.append("0");
+    }
+    std::reverse(s.begin(), s.end());
+    return s;
+}
+
+std::list<std::string> *insert_addresses(std::list<std::string> *program,
+                                         std::list<data *> *data_list,
+                                         std::list<label *> *labels) {
+    auto ld = new std::list<std::string>;
+    labels = normalized_labels(labels);
+    for (auto i: *program) {
+        auto it_l = std::find(labels->begin(),
+                              labels->end(),
+                              [&](label *A) { return A->name == i; });
+        auto it_d = std::find(data_list->begin(),
+                              data_list->end(),
+                              [&](data *A) { return A->name == i; });
+        if (it_l != labels->end()) {
+            ld->push_back(add_padding(std::to_string((*it_l)->pos)));
+        } else if (it_d != data_list->end()) {
+            ld->push_back(add_padding(std::to_string((*it_d)->pos)));
+        } else {
+            ld->push_back(i);
+        }
+    }
+    for (auto i: *labels) {
+        delete i;
+    }
+    for (auto i: *data_list) {
+        delete i;
+    }
+    delete program, labels, data_list;
+    return ld;
 }
