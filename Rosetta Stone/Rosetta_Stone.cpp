e81d6b69c672d8d388483dba3decd414d2f3091b
Zero <dinismatej1@gmail.com>
2022-01-01 02:27:31 +0100

    Fully working compiler.

--- Rosetta Stone/Rosetta_Stone.cpp
+++ Rosetta Stone/Rosetta_Stone.cpp
@@ -1,230 +1,260 @@
-#include "Rosetta_Stone.h"
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <algorithm>
+#include <map>
 
-Lexer::Lexer(const std::string &fd) {
-    m_instructions = new std::list<Token *>;
-    m_variables = new std::list<Token *>;
-    m_in.open(fd);
-    m_OP_codes["NOP"] = 0;
-    m_OP_codes["CLX"] = 1;
-    m_OP_codes["CLY"] = 2;
-    m_OP_codes["CLA"] = 3;
-    m_OP_codes["OUX"] = 4;
-    m_OP_codes["OUY"] = 5;
-    m_OP_codes["OUA"] = 6;
-    m_OP_codes["CMX"] = 16;
-    m_OP_codes["CMY"] = 17;
-    m_OP_codes["CMV"] = 19;
-    m_OP_codes["CALL"] = 20;
-    m_OP_codes["STX"] = 21;
-    m_OP_codes["STY"] = 22;
-    m_OP_codes["STA"] = 23;
-    m_OP_codes["JZ"] = 32;
-    m_OP_codes["JNZ"] = 33;
-    m_OP_codes["JN"] = 34;
-    m_OP_codes["JL"] = 35;
-    m_OP_codes["JG"] = 36;
-    m_OP_codes["JE"] = 37;
-    m_OP_codes["RET"] = 38;
-    m_OP_codes["HLT"] = 39;
-    m_OP_codes["ADX"] = 48;
-    m_OP_codes["ADY"] = 49;
-    m_OP_codes["ADA"] = 50;
-    m_OP_codes["ADR"] = 51;
-    m_OP_codes["ADV"] = 52;
-    m_OP_codes["SUX"] = 64;
-    m_OP_codes["SUY"] = 65;
-    m_OP_codes["SUA"] = 66;
-    m_OP_codes["SUR"] = 67;
-    m_OP_codes["SUV"] = 68;
-    m_OP_codes["LDX"] = 80;
-    m_OP_codes["LDY"] = 81;
-    m_OP_codes["LDA"] = 82;
-    m_OP_codes["LXR"] = 83;
-    m_OP_codes["LYR"] = 84;
-    m_OP_codes["LAR"] = 85;
-    m_OP_codes["MXY"] = 96;
-    m_OP_codes["MXA"] = 97;
-    m_OP_codes["MYX"] = 98;
-    m_OP_codes["MYA"] = 99;
-    m_OP_codes["MAX"] = 100;
-    m_OP_codes["MAY"] = 101;
-    m_OP_codes["ANX"] = 112;
-    m_OP_codes["ANY"] = 113;
-    m_OP_codes["ANV"] = 115;
-    m_OP_codes["ORX"] = 116;
-    m_OP_codes["ORY"] = 117;
-    m_OP_codes["ORV"] = 119;
-    m_OP_codes["XOX"] = 128;
-    m_OP_codes["XOY"] = 129;
-    m_OP_codes["XOV"] = 131;
-    m_OP_codes["NOX"] = 132;
-    m_OP_codes["NOY"] = 133;
-    m_OP_codes["NOA"] = 134;
-    m_OP_codes["NOV"] = 135;
+struct section {
+    std::list<std::string> *data;
+    std::list<std::string> *program;
+};
+
+struct data {
+    std::string name;
+    int pos;
+};
+
+struct label {
+    std::string name;
+    int pos;
+};
+
+struct op_code {
+    int num;
+    int size;
+};
+
+std::map<std::string, op_code> *create_OP_map();
+
+std::list<std::string> *normalized_and_split(std::ifstream *file);
+
+section split(std::list<std::string> *file);
+
+std::list<std::string> *find_labels(std::list<std::string> *program);
+
+std::list<data *> *give_data_pos(std::list<std::string> *dataL);
+
+std::list<label *> *give_label_pos(std::list<std::string> *program,
+                                   std::map<std::string, op_code> *OPs);
+
+std::list<std::string> *remove_labels(std::list<std::string> *program,
+                                      std::list<label *> *label_list);
+
+std::list<label *> *normalized_labels(std::list<label *> *label_list);
+
+std::string add_padding(std::string s);
+
+std::list<std::string> *insert_addresses(std::list<std::string> *program,
+                                         std::list<data *> *data_list,
+                                         std::list<label *> *label_list);
+
+std::list<std::string> *create_output(std::list<std::string> *program,
+                                      std::map<std::string, op_code> *OPs);
+
+void output_for_arduino(std::list<std::string> *program);
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cout << "No input file." << std::endl;
+        return -1;
+    }
+//    bool opt = false;
+//    if (argc == 3) {
+//        opt = true;
+//    }
+
+    auto input_file = new std::ifstream;
+    // "..\\test.asm" argv[1]
+    input_file->open(argv[1]);
+
+    auto[data_l, program_l] = split(normalized_and_split(input_file));
+    output_for_arduino(create_output(insert_addresses(remove_labels(program_l, give_label_pos(program_l, create_OP_map())),
+                                            give_data_pos(data_l), give_label_pos(program_l, create_OP_map())),
+                       create_OP_map()));
+
+    return 0;
 }
 
-Lexer::~Lexer() {
-    delete m_instructions, m_variables;
-    m_in.close();
+std::map<std::string, op_code> *create_OP_map() {
+    auto *map = new std::map<std::string, op_code>;
+    map->insert({"NOP",  {0x80, 1}});
+    map->insert({"CLX",  {0x81, 1}});
+    map->insert({"CLY",  {0x82, 1}});
+    map->insert({"CLA",  {0x83, 1}});
+    map->insert({"OUX",  {0x84, 1}});
+    map->insert({"OUY",  {0x85, 1}});
+    map->insert({"OUA",  {0x86, 1}});
+    map->insert({"CMX",  {0x88, 1}});
+    map->insert({"CMY",  {0x89, 1}});
+    map->insert({"CMA",  {0x8A, 1}}); // why??? constant EQ flag
+    map->insert({"CMV",  {0x8B, 2}});
+    map->insert({"CALL", {0x8C, 3}});
+    map->insert({"STX",  {0x8D, 3}});
+    map->insert({"STY",  {0x8E, 3}});
+    map->insert({"STA",  {0x8F, 3}});
+    map->insert({"JZ",   {0x90, 3}});
+    map->insert({"JNZ",  {0x91, 3}});
+    map->insert({"JN",   {0x92, 3}});
+    map->insert({"JL",   {0x93, 3}});
+    map->insert({"JG",   {0x94, 3}});
+    map->insert({"JE",   {0x95, 3}});
+    map->insert({"RET",  {0x96, 1}});
+    map->insert({"HLT",  {0x97, 1}});
+    map->insert({"ADX",  {0x98, 1}});
+    map->insert({"ADY",  {0x99, 1}});
+    map->insert({"ADA",  {0x9A, 1}});
+    map->insert({"ADI",  {0x9B, 3}});
+    map->insert({"ADV",  {0x9C, 2}});
+    map->insert({"SUX",  {0xA0, 1}});
+    map->insert({"SUY",  {0xA1, 1}});
+    map->insert({"SUA",  {0xA2, 1}}); // same as CLA
+    map->insert({"SUI",  {0xA3, 3}});
+    map->insert({"SUV",  {0xA4, 2}});
+    map->insert({"LDX",  {0xA8, 2}});
+    map->insert({"LDY",  {0xA9, 2}});
+    map->insert({"LDA",  {0xAA, 2}});
+    map->insert({"LDXI", {0xAB, 3}});
+    map->insert({"LDYI", {0xAC, 3}});
+    map->insert({"LDAI", {0xAD, 3}});
+    map->insert({"MXY",  {0xB0, 1}});
+    map->insert({"MXA",  {0xB1, 1}});
+    map->insert({"MYX",  {0xB2, 1}});
+    map->insert({"MYA",  {0xB3, 1}});
+    map->insert({"MAX",  {0xB4, 1}});
+    map->insert({"MAY",  {0xB5, 1}});
+    map->insert({"ANX",  {0xB8, 1}});
+    map->insert({"ANY",  {0xB9, 1}});
+    map->insert({"ANA",  {0xBA, 1}}); // why????
+    map->insert({"ANV",  {0xBB, 2}});
+    map->insert({"ORX",  {0xBC, 1}});
+    map->insert({"ORY",  {0xBD, 1}});
+    map->insert({"ORA",  {0xBE, 1}}); // why????
+    map->insert({"ORV",  {0xBF, 2}});
+    map->insert({"XOX",  {0x40, 1}});
+    map->insert({"XOY",  {0x41, 1}});
+    map->insert({"XOA",  {0x42, 1}}); // why????
+    map->insert({"XOV",  {0x43, 2}});
+    map->insert({"NOX",  {0x44, 1}});
+    map->insert({"NOY",  {0x45, 1}});
+    map->insert({"NOA",  {0x46, 1}});
+    map->insert({"NOV",  {0x47, 2}}); // why????
+    return map;
 }
 
-void Lexer::split() {
-    std::string buffer((std::istreambuf_iterator<char>(m_in)),
+std::list<std::string> *normalized_and_split(std::ifstream *file) {
+    auto ld = new std::list<std::string>;
+    std::string buffer((std::istreambuf_iterator<char>(*file)),
                        (std::istreambuf_iterator<char>()));
-    unsigned int file_size = buffer.length();
-    for (int i = 0; i < file_size - 1; ++i) {
+
+    for (int i = 0; i < buffer.length(); ++i) {
         if (buffer[i] == '\n') {
             buffer[i] = ' ';
             i--;
         }
         if (buffer[i] == ' ' && buffer[i + 1] == ' ') {
-            buffer.erase(i + 1, 1);
+            buffer.erase(i, 1);
             i--;
         }
     }
     if (buffer[0] == ' ') {
         buffer.erase(0, 1);
     }
-    buffer.shrink_to_fit();
     for (int i = 0; i < buffer.length(); ++i) {
         if (i == buffer.length() - 1) {
-            m_file.push_back(buffer.substr(0, buffer.length()));
+            ld->push_back(buffer.substr(0, buffer.length()));
         } else if (buffer[i] == ' ') {
-            m_file.push_back(buffer.substr(0, i));
+            ld->push_back(buffer.substr(0, i));
             buffer.erase(0, i + 1);
             i = 0;
         }
     }
+    return ld;
 }
 
-bool Lexer::isOpcode(const std::string &s) {
-    return (m_OP_codes.find(s) != m_OP_codes.end());
+section split(std::list<std::string> *file) {
+    section st{
+            .data = new std::list<std::string>,
+            .program = new std::list<std::string>
+    };
+    st.data->splice(st.data->begin(),
+                    *file,
+                    ++file->begin(),
+                    (std::find(file->begin(), file->end(), ".program:")));
+    st.program->splice(st.program->begin(),
+                       *file,
+                       ++(std::find(file->begin(), file->end(), ".program:")),
+                       file->end());
+    delete file;
+    return st;
 }
 
-int Lexer::getOpcodeNumber(const std::string &s) {
-    return m_OP_codes[s];
-}
-
-bool Lexer::isAddress(const std::string &s) {
-    return (s.find('$') != std::string::npos);
-}
-
-bool Lexer::isLabel(const std::string &s) {
-    return (s.find(':') != std::string::npos);
-}
-
-bool Lexer::isNumber(const std::string &s) {
-    return (s[0] >= 48 && s[0] <= 57);
-}
-
-int Lexer::getVariablePos(const std::string &s) {
-    for (auto i: *m_variables) {
-        if (i->m_variable == s) {
-            return i->m_var_pos;
+std::list<std::string> *find_labels(std::list<std::string> *program) {
+    auto *list = new std::list<std::string>;
+    for (const auto &i: *program) {
+        if (i.find(':') != std::string::npos) {
+            list->push_back(i);
         }
     }
-    throw std::invalid_argument("Var doesnt exist.\n");
+    return list;
 }
 
-void Lexer::tokenized() {
-    this->split();
-    Token *token = nullptr;
-    bool data = false;
-    int ram_pos = 0;
-    for (auto i = m_file.begin();
-         i != m_file.end();
-         i++) {
-        if (*i == ".data:") {
-            data = true;
-        } else if (*i == ".program:") {
-            data = false;
-        } else {
-            token = new Token;
-            if (data) {
-                token->type = token_type::VARIABLE;
-                token->m_variable = *i;
-                token->m_var_pos = ram_pos;
-                ram_pos++;
-                m_variables->push_back(token);
-            } else {
-                if (isOpcode(*i)) {
-                    token->type = token_type::OPCODE;
-                    token->m_opcode = getOpcodeNumber(*i);
-                    token->m_size = 1;
-                    if (token->m_opcode == 83 ||
-                        token->m_opcode == 84 ||
-                        token->m_opcode == 85) {
-                        i++;
-                        token->m_variable = *i;
-                        token->m_var_pos = getVariablePos(token->m_variable);
-                    }
-                } else if (isAddress(*i)) {
-                    token->type = token_type::ADDRESS;
-                    std::string s = i->substr(1, i->length());
-                    token->m_addressH = std::stoi(s.substr(0, 2),
-                                                  nullptr, 16);
-                    token->m_addressL = std::stoi(s.substr(2, s.length()),
-                                                  nullptr, 16);
-                    token->m_size = 2;
-                } else if (isNumber(*i)) {
-                    token->type = token_type::NUMBER;
-                    token->m_number = std::stoi(*i, nullptr, 16);
-                    token->m_size = 1;
-                } else if (isLabel(*i)) {
-                    token->type = token_type::LABEL_TO;
-                    token->m_label = i->substr(0, i->length() - 1);
-                    token->m_size = 0;
-                } else {
-                    token->type = token_type::LABEL_FROM;
-                    token->m_label = *i;
-                    token->m_size = 0;
-                }
-                m_instructions->push_back(token);
-            }
+std::list<data *> *give_data_pos(std::list<std::string> *dataL) {
+    auto *data_list = new std::list<data *>;
+    int p = 0;
+    for (const auto &i: *dataL) {
+        data_list->push_back(new data{
+                .name = i,
+                .pos = p
+        });
+        p++;
+    }
+    return data_list;
+}
+
+std::list<label *> *give_label_pos(std::list<std::string> *program,
+                                   std::map<std::string, op_code> *OPs) {
+    auto *list = new std::list<label *>;
+    auto *labels = find_labels(program);
+    int index = 0;
+    for (const auto &i: *program) {
+        auto it_label = std::find_if(labels->begin(),
+                                     labels->end(),
+                                     [&](const std::string &A) { return i == A; });
+        auto it_op = OPs->find(i);
+        if (it_label != labels->end()) {
+            list->push_back(new label{
+                    .name = (*it_label),
+                    .pos = index
+            });
+        } else if (it_op != OPs->end()) {
+            index += it_op->second.size;
         }
     }
+    return list;
 }
 
-std::list<Token *> *Lexer::getInstructions() {
-    return m_instructions;
-}
-
-Rosetta_Stone::Rosetta_Stone(bool o) {
-    O3 = o;
-    m_instructions = nullptr;
-    m_file = new std::list<unsigned char>;
-}
-
-Rosetta_Stone::~Rosetta_Stone() {
-    delete m_instructions, m_file;
-}
-
-void Rosetta_Stone::setInstructions(std::list<Token *> *l) {
-    m_instructions = l;
-}
-
-void Rosetta_Stone::findLabels() {
-    int start = 0;
-    for (auto i: *m_instructions) {
-        if (i->type == token_type::LABEL_TO) {
-            m_labels.emplace_back(i, start);
-        } else {
-            start += i->m_size;
+std::list<std::string> *remove_labels(std::list<std::string> *program,
+                                      std::list<label *> *label_list) {
+    auto *new_ld = new std::list<std::string>;
+    for (auto i: *program) {
+        auto it_label = std::find_if(label_list->begin(),
+                                     label_list->end(),
+                                     [&](label *A) { return i == A->name; });
+        if (it_label == label_list->end()) {
+            new_ld->push_back(i);
         }
     }
+    return new_ld;
 }
 
-int Rosetta_Stone::labelPos(const std::string &s) {
-    for (auto i: m_labels) {
-        if (i.first->m_label == s) {
-            return i.second;
-        }
+std::list<label *> *normalized_labels(std::list<label *> *label_list) {
+    for (auto i: *label_list) {
+        i->name = i->name.substr(0, i->name.length() - 1);
     }
-    std::string a = "Label " + s + " doesn't exist.\n";
-    throw std::invalid_argument(s);
+    return label_list;
 }
 
-std::string Rosetta_Stone::addPadding(std::string s) {
+std::string add_padding(std::string s) {
     std::reverse(s.begin(), s.end());
     while (s.length() != 4) {
         s.append("0");
@@ -233,77 +263,69 @@ std::string Rosetta_Stone::addPadding(std::string s) {
     return s;
 }
 
-std::string Rosetta_Stone::toHex(int n) {
-    std::map<int, char> m;
-    m[0] = '0';
-    m[1] = '1';
-    m[2] = '2';
-    m[3] = '3';
-    m[4] = '4';
-    m[5] = '5';
-    m[6] = '6';
-    m[7] = '7';
-    m[8] = '8';
-    m[9] = '9';
-    m[10] = 'A';
-    m[11] = 'B';
-    m[12] = 'C';
-    m[13] = 'D';
-    m[14] = 'E';
-    m[15] = 'F';
-    std::string r;
-    if (n > 0) {
-        while (n) {
-            r += m[n % 16];
-            n /= 16;
-        }
-    }
-    std::reverse(r.begin(), r.end());
-    return r;
-}
-
-void Rosetta_Stone::optimized() {
-    this->findLabels();
-    if (O3) {
-        for (auto it = m_instructions->begin();
-             it != m_instructions->end();
-             it++) {
-        }
-    } else {
-        std::string a;
-        for (auto i: *m_instructions) {
-            if (i->type == token_type::OPCODE) {
-                m_file->push_back(i->m_opcode);
-                if (i->m_opcode == 83 ||
-                    i->m_opcode == 84 ||
-                    i->m_opcode == 85) {
-                    a = addPadding(std::to_string(i->m_var_pos));
-                    m_file->push_back(std::stoi(a.substr(2, a.length()),
-                                                nullptr, 16));
-                    m_file->push_back(std::stoi(a.substr(0, 2),
-                                                nullptr, 16));
-                }
-            } else if (i->type == token_type::NUMBER) {
-                m_file->push_back(i->m_number);
-            } else if (i->type == token_type::ADDRESS) {
-                m_file->push_back(i->m_addressL);
-                m_file->push_back(i->m_addressH);
-            } else if (i->type == token_type::LABEL_FROM) {
-                a = addPadding(toHex(labelPos(i->m_label)));
-                m_file->push_back(std::stoi(a.substr(2, a.length()),
-                                            nullptr, 16));
-                m_file->push_back(std::stoi(a.substr(0, 2),
-                                            nullptr, 16));
+std::list<std::string> *insert_addresses(std::list<std::string> *program,
+                                         std::list<data *> *data_list,
+                                         std::list<label *> *label_list) {
+    auto ld = new std::list<std::string>;
+    label_list = normalized_labels(label_list);
+    bool A = false;
+    for (const auto& i: *program) {
+        for (auto l: *label_list) {
+            if (l->name == i) {
+                ld->push_back(add_padding(std::to_string(l->pos)));
+                A = true;
+                break;
             }
         }
+        for (auto d: *data_list) {
+            if (d->name == i) {
+                ld->push_back(add_padding(std::to_string(d->pos)));
+                A = true;
+                break;
+            }
+        }
+        if (!A) {
+            ld->push_back(i);
+        }
+        A = false;
     }
+    for (auto i: *label_list) {
+        delete i;
+    }
+    for (auto i: *data_list) {
+        delete i;
+    }
+    return ld;
 }
 
-void Rosetta_Stone::compile() {
-    m_out.open("out.hex", std::ios::binary | std::ios::out);
-    for (auto i: *m_file) {
-        m_out << i;
-        std::cout<<std::hex<<i<<",";
+std::list<std::string> *create_output(std::list<std::string> *program,
+                                      std::map<std::string, op_code> *OPs) {
+    auto ld = new std::list<std::string>;
+    for (const auto& i: *program) {
+        if (OPs->find(i) != OPs->end()) {
+            ld->push_back(std::to_string(OPs->find(i)->second.num));
+        } else {
+            ld->push_back(i);
+        }
     }
-    m_out.close();
+    return ld;
+}
+
+void output_for_arduino(std::list<std::string> *program) {
+    std::string out;
+    out.append("int program[] = {");
+    for (const auto& i: *program) {
+        if (i.length() < 4) {
+            out.append(i);
+            out.append(", ");
+        } else {
+            out.append(i.substr(2, 2));
+            out.append(", ");
+            out.append(i.substr(0, 2));
+            out.append(", ");
+        }
+    }
+    out.erase(out.length()-2, 2);
+    out.append("};");
+    std::cout<<out<<std::endl;
 }
